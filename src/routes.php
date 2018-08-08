f3c167591c3c1c0c72b985bf332ab48aa1245107
Donald Pittard <donaldpittard@gmail.com>
2018-08-08 11:03:10 -0400

    Limited regions searched

--- src/routes.php
+++ src/routes.php
@@ -15,7 +15,17 @@ $app->get('/', function (Request $request, Response $response, array $args) {
 });
 
 $app->post('/api/timezones', function (Request $request, Response $response, array $args) {
-    $timeZones   = DateTimeZone::listAbbreviations(DateTimeZone::ALL);
+    $regions     = [
+        'Africa'     => DateTimeZone::AFRICA,
+        'America'    => DateTimeZone::AMERICA,
+        'Antarctica' => DateTimeZone::ANTARCTICA,
+        'Asia'       => DateTimeZone::ASIA,
+        'Atlantic'   => DateTimeZone::ATLANTIC,
+        'Europe'     => DateTimeZone::EUROPE,
+        'Indian'     => DateTimeZone::INDIAN,
+        'Pacific'    => DateTimeZone::PACIFIC
+    ];
+    $timeZones   = [];
     $searchTerm  = $_POST['term'];
     $suggestions = [];
 
@@ -26,18 +36,20 @@ $app->post('/api/timezones', function (Request $request, Response $response, arr
         return $response->withJson([]);
     }
 
-    foreach ($timeZones as $abbr => $timeZoneInfo) {
-        $id = $timeZoneInfo[0]['timezone_id'];
+    foreach ($regions as $regionName => $regionId) {
+        $regionTimeZones = DateTimeZone::listIdentifiers($regionId);
+        $this->logger->info($regionId);
+        $timeZones = array_merge($timeZones, $regionTimeZones);
+    }
 
-        if (!$id) {
-            continue;
-        }
-
+    foreach ($timeZones as $id) {
         $dateTimeZone = new DateTimeZone($id);
+        $dateTime     = new DateTime(null, $dateTimeZone);
         $name         = preg_replace('/_/', ' ', $dateTimeZone->getName());
         $location     = $dateTimeZone->getLocation();
         $country      = $location['country_code'];
         $comments     = $location['comments'];
+        $abbr         = $dateTime->format('T');
 
         if (stripos($name, $searchTerm) > -1 ||
             stripos($abbr, $searchTerm) > -1 ||
